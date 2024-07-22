14a8783fcb515f2237a81f255ae5cfb8683b2317
cmyhj <2484684043@qq.com>
2024-07-22 20:04:28 +0800

    rm diankong

--- balance_car/App/pid.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PID_H__
-#define __PID_H__
-
-float balance(float Angle,float Gyro);
-float Velocity(float Encoder_A,float Encoder_B,float Mechanical_velocity);
-
-#endif
--- balance_car/MDK-ARM/balance_car/pid.d
+++
@@ -1,2 +0,0 @@
-balance_car\pid.o: ..\App\pid.c
-balance_car\pid.o: ..\App\pid.h
